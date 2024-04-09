218f64cdf27a2309b593e31a069c315a197b5cd5
Aiweline <32635766+Aiweline@users.noreply.github.com>
2024-04-09 06:26:32 +0000

    优化权限日志名称

--- Observer/BackendControllerInit.php
+++ Observer/BackendControllerInit.php
@@ -39,6 +39,7 @@ class BackendControllerInit implements ObserverInterface
         # ACL访问列表信息
         $acl = $this->acl->where(Acl::fields_CLASS, $this->request->getRouterData('class/name'))
             ->where(Acl::fields_METHOD, $this->request->getMethod())
+            ->where(Acl::fields_ROUTE, $this->request->getRouteUrlPath())
             ->find()
             ->fetch();
         if (!$acl->getId()) {
